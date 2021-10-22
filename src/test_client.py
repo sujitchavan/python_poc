a63a467b69830ce17dbcb6e34b3b76ff5f99b549
sujitchavan <sujitchavan22@gmail.com>
2021-10-22 18:02:59 +0530

    second commit

--- src/test_client.py
+++ src/test_client.py
@@ -7,14 +7,14 @@ import Registry
 import win32serviceutil
 import File
 
-def download_client():
+def test_downloadclient():
     print('Downloading client')
     url = 'https://downloads.hpdaas.com/master/microsoft/tm-client/3.21.1239/HPTechPulse.msi'
     r = requests.get(url, allow_redirects=True)
     open('HPTechPulse.msi', 'wb').write(r.content)
     print('Downloaded')
 
-def install_enroll_client():
+def test_installenrollclient():
     print('Installing and Enrolling client')
     subprocess.call('msiexec /i %s CPIN=%s SERVER=%s' % ('HPTechPulse.msi', 'S44XiHmq', 'https://usdevms.daas.hppipeline.com/'), shell=True)
     print('Installation is done and enrolling client, waiting for 1 min 30 seconds')
@@ -29,13 +29,13 @@ def install_enroll_client():
 
 
 # AwsIot:Publish event success, Event: DeviceSBChanged
-def verify_devicesbchangeevent_onenroll():
+def test_devicesbchangeeventonenroll():
     File.search_text_in_file('C:\\ProgramData\\HP\\StreamLog\\LHAgent.exe\\*.log', 
         "AwsIot:Publish event success, Event: DeviceSBChanged", 
         "DeviceSbChanged")
 
 
-def verify_devicesbchangeevent_notsent_onservicerestart():
+def test_devicesbchangeeventnotsentonservicerestart():
     # Restart LHAgent service
     print('Restarting LHAgent service')
     win32serviceutil.RestartService("hpLHAgent")
@@ -48,10 +48,23 @@ def verify_devicesbchangeevent_notsent_onservicerestart():
         "DeviceSbChanged")
 
 
+def test_bitlockereventsentonservicerestart():
+    # Restart LHAgent service
+    print('Restarting LHAgent service')
+    win32serviceutil.RestartService("hpLHAgent")
+
+    print("Restarted, Waiting for 5 mins")
+    time.sleep(300)
+
+    File.search_text_in_file('C:\\ProgramData\\HP\\StreamLog\\LHAgent.exe\\*.log', 
+        "AwsIot:Publish event success, Event: bitlocker-status-event", 
+        "bitlocker-status-event")
+
 # print('Starting file watcher')
 # Filewatcher.file_watcher()
 # latest_file = Filewatcher.get_latest_filename()
 # download_client()
 # install_enroll_client()
 # verify_devicesbchangeevent_onenroll()
-verify_devicesbchangeevent_notsent_onservicerestart()
+# verify_devicesbchangeevent_notsent_onservicerestart()
+# verify_bitlockerevent_sent_onservicerestart()
