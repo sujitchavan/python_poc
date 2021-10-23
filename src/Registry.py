c91edc786fbe1a6095c96fed3f56a0e59d85ee28
sujitchavan <sujitchavan22@gmail.com>
2021-10-23 12:17:04 +0530

    3

--- src/Registry.py
+++ src/Registry.py
@@ -10,5 +10,20 @@ def read_registry(name):
         winreg.CloseKey(registry_key)
         print(value)
         return value
-    except WindowsError:
+    except:
+        return None
+
+def write_registry(path, key, value):
+    try:
+        reg_key = winreg.CreateKey(winreg.HKEY_LOCAL_MACHINE, path)
+        winreg.SetValueEx(reg_key, key, 0, winreg.REG_SZ, value)
+        winreg.CloseKey(reg_key)
+    except:
+        return None
+    
+
+def delete_registry(Path):
+    try:
+        winreg.DeleteKey(winreg.HKEY_LOCAL_MACHINE, path)
+    except:
         return None
--- src/test_client.py
+++ src/test_client.py
@@ -7,14 +7,19 @@ import Registry
 import win32serviceutil
 import File
 
-def test_downloadclient():
+
+def downloadclient():
     print('Downloading client')
     url = 'https://downloads.hpdaas.com/master/microsoft/tm-client/3.21.1239/HPTechPulse.msi'
     r = requests.get(url, allow_redirects=True)
     open('HPTechPulse.msi', 'wb').write(r.content)
     print('Downloaded')
 
+
+# Capability Check: Web Req download, MSI Installation, Windows Registry API
 def test_installenrollclient():
+    Registry.delete_registry(r'SOFTWARE\Policies\Hewlett-Packard\HPTechPulse\AssetLocation')
+    downloadclient()
     print('Installing and Enrolling client')
     subprocess.call('msiexec /i %s CPIN=%s SERVER=%s' % ('HPTechPulse.msi', 'S44XiHmq', 'https://usdevms.daas.hppipeline.com/'), shell=True)
     print('Installation is done and enrolling client, waiting for 1 min 30 seconds')
@@ -24,30 +29,31 @@ def test_installenrollclient():
     val = Registry.read_registry('DeviceEnrolled')
     if(val == "True"):
         print("Successfully enrolled")
+        print("First Test Case Passed")
+        assert True
     else:
         print('Failed to enrolled')
+        print("First Test Case Failed")
+        assert False
 
 
-# AwsIot:Publish event success, Event: DeviceSBChanged
+# Capability Check: File Operartions
 def test_devicesbchangeeventonenroll():
-    File.search_text_in_file('C:\\ProgramData\\HP\\StreamLog\\LHAgent.exe\\*.log', 
+    time.sleep(150)
+
+    val = File.search_text_in_file('C:\\ProgramData\\HP\\StreamLog\\LHAgent.exe\\*.log', 
         "AwsIot:Publish event success, Event: DeviceSBChanged", 
         "DeviceSbChanged")
 
-
-def test_devicesbchangeeventnotsentonservicerestart():
-    # Restart LHAgent service
-    print('Restarting LHAgent service')
-    win32serviceutil.RestartService("hpLHAgent")
-
-    print("Restarted, Waiting for 5 mins")
-    time.sleep(300)
-
-    File.search_text_in_file('C:\\ProgramData\\HP\\StreamLog\\LHAgent.exe\\*.log', 
-        "AwsIot:Publish event success, Event: DeviceSBChanged", 
-        "DeviceSbChanged")
+    if(val == True):
+        assert True
+        print("second Test Case Passed")
+    else:
+        assert False
+        print("second Test Case Failed")
 
 
+# Capability Check: Windows Service API
 def test_bitlockereventsentonservicerestart():
     # Restart LHAgent service
     print('Restarting LHAgent service')
@@ -56,15 +62,36 @@ def test_bitlockereventsentonservicerestart():
     print("Restarted, Waiting for 5 mins")
     time.sleep(300)
 
-    File.search_text_in_file('C:\\ProgramData\\HP\\StreamLog\\LHAgent.exe\\*.log', 
+    val = File.search_text_in_file('C:\\ProgramData\\HP\\StreamLog\\LHAgent.exe\\*.log', 
         "AwsIot:Publish event success, Event: bitlocker-status-event", 
         "bitlocker-status-event")
 
-# print('Starting file watcher')
-# Filewatcher.file_watcher()
-# latest_file = Filewatcher.get_latest_filename()
-# download_client()
-# install_enroll_client()
-# verify_devicesbchangeevent_onenroll()
-# verify_devicesbchangeevent_notsent_onservicerestart()
-# verify_bitlockerevent_sent_onservicerestart()
+    if(val == True):
+        assert True
+        print("Third Test Case Passed")
+    else:
+        assert False
+        print("Third Test Case Failed")
+
+
+# device heirachy location event on change validation and data validation
+def test_deviceheirarchylocation():
+    Registry.write_registry(r'SOFTWARE\Policies\Hewlett-Packard\HPTechPulse\AssetLocation', 
+        'DeviceLocation', 
+        'Pune\\baner\\AmarApex\\HP1')
+    
+    time.sleep(5)
+
+    val = File.search_text_in_file('C:\\ProgramData\\HP\\StreamLog\\LHAgent.exe\\*.log', 
+        "AwsIot:Publish event success, Event: DeviceHierarchyLocationUpdated", 
+        "DeviceHierarchyLocationUpdated")    
+
+    if(val == True):
+        print("Fourth Test Case Passed")
+        assert True
+    else:
+        print("Fourth Test Case Failed")
+        assert False
+
+
+# event viewer : client crash validation
